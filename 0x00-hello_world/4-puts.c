fc81221e1eb5f17cf65315ee2286b8248865a4e7
Esoteric918 <3411@hobertonschool.com>
2021-06-23 21:12:45 +0000

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
-#!/bin/bash
-int<stdio.h>
-puts (const char *Programming is like building a multilingual puzzle); return (0)
+#include <stdio.h>
+/**
+ * main - uses puts to print string and Return 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
