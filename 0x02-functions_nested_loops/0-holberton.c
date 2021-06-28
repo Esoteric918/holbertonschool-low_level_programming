0e7acf883167943cb9b0620eae2188ffa31d5822
Esoteric918 <3411@hobertonschool.com>
2021-06-28 17:57:48 +0000

    print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton"
+/**
+ * main - Print Holberton
+ * Return: 0
+ */
+int main(void)
+{
+	char string[] = "Holberton";
+		int i = 0;
+	while (string[i])
+	{
+		_putchar(string[i]);
+		++i;
+	}
+	_putchar('\n');
+	return (0);
+}
