e5107a7972b5268791d3626bfb890eef6a9938e4
Esoteric918 <3411@hobertonschool.com>
2021-07-01 19:06:06 +0000

    is ABC uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_isupper - to check for uppercase
+ *@c: int looking for _supper
+ *Return: 0
+ */
+int _isupper(int c)
+{
+	
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+	
+}
