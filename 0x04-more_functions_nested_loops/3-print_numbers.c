612ce081cfa03af6b2ba8406493faefca820b235
Esoteric918 <3411@hobertonschool.com>
2021-07-01 21:26:21 +0000

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_numbers - print the number 0 to 9
+ *
+ *Return: 0
+ */
+void print_numbers(void)
+{
+	char g;
+
+	for (g = 0 && g <= 9 ; ++g)
+	{
+		_putchar(g);
+	}
+	return (0);
+}
