f344decc908c2746d5563bc114eb3620b4722b74
Esoteric918 <3411@hobertonschool.com>
2021-07-01 15:20:05 +0000

    fix bug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,13 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = a;
 
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b >= c && b >= a)
 	{
 		largest = b;
 	}
