618b85b1f381e5fad596bffe3a3c477cac0327c2
Esoteric918 <3411@hobertonschool.com>
2021-07-02 13:23:37 +0000

    10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *more_numbers - 0 to 14 10x
+ *
+ *Return: 0
+ */
+void more_numbers(void)
+{
+	int p;
+	int d;
+
+	while (d < 10)
+	{
+		for (p = 0; p < 15; ++p)
+		{
+			if (p > 9)
+			{
+				_putchar('1');
+			}
+			else
+				_putchar(p % 10 + '0');
+		}
+		_putchar('\n');
+		++d;
+	}
+}
