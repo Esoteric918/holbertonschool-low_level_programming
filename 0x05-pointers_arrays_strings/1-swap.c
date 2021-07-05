582b2e5f8e1ef2fe669122a1058e80d8e2abd3ef
Esoteric918 <3411@hobertonschool.com>
2021-07-05 20:23:27 +0000

    swap one int for another

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- *swap_int - swap one int val for another
- *@p: int to be swapped from 
- *@d: int to be swapped to 
- *Return: void
- */
+*swap_int - swap one int val for another
+*@p: int to be swapped from
+*@d: int to be swapped to
+*Return: void
+*/
 void swap_int(int *p, int *d)
 {
 	int temp;
 
-	temp =* p;
-	*p    = *d;
-	*d    = temp;
+	temp = *p;
+	*p = *d;
+	*d = temp;
 }
