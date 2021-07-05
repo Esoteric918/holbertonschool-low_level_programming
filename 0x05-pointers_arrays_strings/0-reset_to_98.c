2949b24aa9ec0913ccb7e900ac26be19368c027c
Esoteric918 <3411@hobertonschool.com>
2021-07-05 19:58:23 +0000

    pointer n reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *reset_to_98 -  input to pointer to an int rtn int to 98
+ *@n: pointer to an int value to be reset
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
