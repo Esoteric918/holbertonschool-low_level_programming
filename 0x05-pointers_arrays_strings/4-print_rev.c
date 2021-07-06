f10d1b6465c1d2a3a72baf9fbf3064c03d91ac79
Esoteric918 <3411@hobertonschool.com>
2021-07-06 14:22:10 +0000

    print string in rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,19 +2,18 @@
 
 /**
  * print_rev - prints a string, in reverse
- * @s: string to be reversed
+ * @str: string to be reversed
  *
  * Return: void
  */
 void print_rev(char *str)
 {
 	int k = 0;
-	
+
 	while (str[k] != '\0')
 	{
 		++k;
 	}
-	
 	for (k--; k >= 0; k--)
 	{
 		_putchar(str[k]);
