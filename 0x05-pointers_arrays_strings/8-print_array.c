e0ed3824802afd5fbb631464ab9e5fc29e301a7b
Esoteric918 <3411@hobertonschool.com>
2021-07-07 18:58:49 +0000

    pointer at array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - pointer at array
+*@a: pointer
+*@n: elements of an array of integers
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		if (k != n - 1)
+			printf("%d, ", a[k]);
+		else
+			printf("%d\n", a[k]);
+	}
+}
