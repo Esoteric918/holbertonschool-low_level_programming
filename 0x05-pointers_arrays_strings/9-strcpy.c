416f2e2d0506e99eec22b99161a40d87cc8ecd7b
Esoteric918 <3411@holbertonschool.com>
2021-07-07 14:57:33 -0700

    coppy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- *_strcpy - copy string 
- *@dest: address pointed to 
- *@src: string being pointed to 
- *Return
- */
+*_strcpy - copy string 
+*@dest: address pointed to 
+*@src: string being pointed to 
+*Return
+*/
 char *_strcpy(char *dest, char *src)
 {
-  int k;
-  
-  while((dest[k] = src[k]) != '\0')
-    {
-      ++k;
-    }
+int k = 0;
+
+ while ((dest[k] = src[k]) != '\0')
+   {
+     ++k;
+   }
   return(dest);
 }
