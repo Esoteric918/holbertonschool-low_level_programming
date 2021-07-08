0fc2dba8bccd89cd002d28d78fe25190ebfa2311
Esoteric918 <3411@hobertonschool.com>
2021-07-08 03:41:16 +0000

    string  to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,29 @@
 #include "holberton.h"
 /**
  * _atoi - convert a string to an int
- * @nstr: pointer to an int value to be reset
+ * @str: pointer to an int value to be reset
+ *
  * Return: void
  */
 int _atoi(char *str)
 {
 	int res;
 	int pu;
+	int k = 0;
 
 	res = 0;
 	pu = 1;
-	
-	while (( '-' == (*str)) || ((*str == '+'))
-		{
-			if (*str == '-')
-				pu = pu * -1;
-			str++;
-		}
-		while ((*str >= '0') && (*str <='9'))
-		{	
-			res = (res * 10) + ((*str) -'0');
-			str++;
-		}
-		return (res * pu);
-		}
+
+	while (str[k] != 0)
+	{
+		if (*str == '-')
+			pu = pu * -1;
+		str++;
+	}
+	while ((*str >= '0') && (*str <= '9'))
+	{
+		res = (res * 10) + ((*str) - '0');
+		str++;
+	}
+	return (res * pu);
 }
