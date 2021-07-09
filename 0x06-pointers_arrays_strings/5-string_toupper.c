ae1375fa2974c8bed0b02571aa87b3c3812e7255
Esoteric918 <3411@hobertonschool.com>
2021-07-09 02:46:48 +0000

    make all caps

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *string_toupper(char *len)
+{
+	int i;
+
+	for (i = 0; len[i] != '\0'; ++i)
+	{
+		if (len[i] >= 'a' && len [i] <= 'z')
+			len[i] = len[i] - 32;
+	}
+	return (len);
+}
