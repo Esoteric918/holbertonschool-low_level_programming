6120c579ec428a48c635a76181526137c696b2ff
Esoteric918 <3411@holbertonschool.com>
2021-07-09 07:47:05 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memset - fills the n bytes of memory
+ *@dest: pointer to the memory area to be filled
+ *@src: the char to fill the memory
+ *@n: the number of bytes to be filled
+ *
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for(k = 0; k < n; k++)
+		dest[k] = src[k];
+	return (dest);
+}
