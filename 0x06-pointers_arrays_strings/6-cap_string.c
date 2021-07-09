7b52cc4bc2bae232db3bbf1f251b2f320af5a9da
Esoteric918 <3411@hobertonschool.com>
2021-07-09 03:48:50 +0000

    cap every word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,19 +9,19 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i=0; str[i]; i++)
+	for (i = 0; str[i]; i++)
 	{
 		if (i == 0)
 		{
-			if ((str[i]>= 'a' && str[i] <= 'z'))
-				str[i]=str[i] - 32;
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
 			continue;
 		}
-		if (str[i]==' ')
+		if (str[i] == ' ')
 		{
-			if(str[i + 1]>= 'a' && str[i + 1]<='z')
+			if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i + 1]=str[i + 1] - 32;
+				str[i + 1] = str[i + 1] - 32;
 				continue;
 			}
 		}
