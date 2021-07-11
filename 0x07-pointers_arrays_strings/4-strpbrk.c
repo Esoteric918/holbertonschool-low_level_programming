a679fb36f2de26c3655547115531aca8072691ba
Esoteric918 <3411@holbertonschool.com>
2021-07-10 21:16:21 -0700

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches  string for any set of bytes
+ *@s: pointer
+ *@accept: string
+ *
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *str = NULL;
+
+	if (s == NULL || accept == NULL)
+	{
+		return (NULL);
+	}
+	while (*s != '\0')
+	{
+		str = accept;
+		while (*str != '\0')
+		{
+			if (*s == *str)
+			{
+				return (s);
+			}
+			str++;
+		}
+		s++;
+	}
+	return (s);
+}
