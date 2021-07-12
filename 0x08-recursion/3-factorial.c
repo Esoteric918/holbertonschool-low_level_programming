1e5ad0dc7511bfb729144a09e8841abedff8bb1d
Esoteric918 <3411@holbertonschool.com>
2021-07-12 12:19:56 -0700

    bug fix

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * factorial - fuctionto return given num
+ *@n: int
+ *
+ *Return: 0
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return(1);
+	}
+	else if (n > 0)
+		return(n * factorial(n - 1));
+	else
+		return(-1);
+}
