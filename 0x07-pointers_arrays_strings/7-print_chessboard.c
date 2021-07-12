39a189e02e272eb7731757debc29b5c1a3e14671
Esoteric918 <3411@holbertonschool.com>
2021-07-11 20:47:44 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints out a 8x8 board of chars
+ * @arr: str to search
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; ++row)
+	{
+		for (col = 1; col < 8; ++col)
+			_putchar(a[row][col]);
+		_putchar('\n');
+	}
+}
