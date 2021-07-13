66eed7bcf09e338ecb6271f8dc72e81ecb155c1e
Esoteric918 <3411@holbertonschool.com>
2021-07-12 19:24:32 -0700

    print sum of diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the summation of Diagonal right, diag left values
+ * @ar: pointer to two dimensional array of size sz
+ * @sz: size of two dimensional array
+ * Return: void
+ */
+void print_diagsums(int *ar, int sz)
+{
+	int col;
+	int dr = 0, dl = 0;
+
+	for (col = 0; col < sz; ++col)
+	{
+		dr += ar[col];
+		dl += ar[sz - col - 1];
+		ar += sz;
+	}
+	printf("%d, %d\n", dr, dl);
+}
