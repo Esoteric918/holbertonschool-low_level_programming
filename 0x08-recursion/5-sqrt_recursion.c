8cd48584de8614934589a50a28975260731d863e
Esoteric918 <3411@holbertonschool.com>
2021-07-13 08:37:34 -0700

    sqrt of int

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+int square(int x, int n);
+
+/**
+ * _sqrt_recursion - function that returns sqrt fo int
+ *@n: int val
+ *
+ *Return: -1
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(1, n));
+}
+/**
+ * square - squares integers
+ * @x: integer from _sqrt_recursion
+ * @n: square root number from _sqrt_recursion
+ *
+ * Return: squared number
+ */
+int square(int x, int n)
+{
+	if (x * x == n)
+		return(x);
+	else if (x * x > n)
+		return(-1);
+	return(square(x + 1, n));
+}
