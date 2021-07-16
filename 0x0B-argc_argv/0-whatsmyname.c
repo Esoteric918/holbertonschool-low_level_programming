9d02261bfba3499f3a2680cc914f42d84e6734fc
Esoteric918 <3411@hobertonschool.com>
2021-07-16 07:55:17 +0000

    file names itself

--- 0x0B-argc_argv/0-whatsmyname.c
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * main - find the length of a string
+ * @argc: an int value defining the number of arguements passed into argv
+ * @argv: an array of null-term str representing cmnd-line args
+ * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
