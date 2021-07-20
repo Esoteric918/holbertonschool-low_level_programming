f2bd86db5e48cdddd42e76f370b215fa2fb03c2f
Esoteric918 <3411@holbertonschool.com>
2021-07-20 10:39:20 -0700

    free the dup

--- 0x0C-malloc_free/1-strdup.c
+++ 0x0C-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strdup -
+ * _strdup - return a dup of a string
  *@str: string to be dup and returned
  *
  *Return: str or NULL if not enough mem
@@ -16,14 +16,14 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	dup = malloc(sizeof(*str));
+	dup = malloc(sizeof(*str) + 1);
 
 	if (dup == NULL)
 	{
-		free (dup);
+		free(dup);
 		return (NULL);
 	}
-	for(i = 0; str[i]; i++)
+	for (i = 0; str[i]; i++)
 	{
 		dup[i] = str[i];
 	}
