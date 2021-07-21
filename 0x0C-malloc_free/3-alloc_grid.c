af5cfc98e1fec21c59844f2bed130f685b23d91f
Esoteric918 <3411@holbertonschool.com>
2021-07-20 20:29:46 -0700

    working the bugs out

---
+++ 0x0C-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns ptr for 2D array
+ *@width: width of grid
+ *@height: of grid
+ *
+ *Return: NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ray, r, c;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ray = malloc(sizeof(int *) * width);
+	if (ray == NULL)
+	{
+		return (ray);
+	}
+	for (r = 0; r < height; ++r)
+	{
+		ray[r] = malloc(sizeof(int *) * height);
+		if (ray[r] == NULL)
+		{
+			for (c = 0;c < r; ++c)
+				free(ray[c]);
+			free(ray);
+			return (NULL);
+		}
+		for (r = 0; r < height; ++r)
+		{
+			for (c = 0; c < width; ++c)
+				ray[r][c] = 0;
+		}
+	}
+	return (ray);
+}
