c50e8acd5d6f8e6d5192c3e47c5bf7a717897a27
Esoteric918 <3411@holbertonschool.com>
2021-07-20 23:30:16 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#ifndef __BASE_FILE__
+#define __BASE_FILE__
+#endif
+/**
+ * main - call function
+ *Return: 0
+ */
+int main()
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
