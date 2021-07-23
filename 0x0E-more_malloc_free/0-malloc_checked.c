f5513b26c8e97d42f8219793d12d020fc32f431b
Esoteric918 <3411@holbertonschool.com>
2021-07-23 12:18:26 -0700

    allocates memory using malloc

---
+++ 0x0E-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check proper use of malloc
+ *@b int used to check
+ *
+ *Return: NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	if (b == 0)
+		return (NULL);
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
