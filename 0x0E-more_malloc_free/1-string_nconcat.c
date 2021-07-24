1b33890c4b9f8f9e9d4726d049d76a430a6071f5
Esoteric918 <3411@holbertonschool.com>
2021-07-23 18:23:59 -0700

    fuck it

---
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two str
+ *@sl: 1st string
+ *@s2: 2nd string
+ *@n: number of chars to add from s2
+ *Return: ptr cat
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	char *cat;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+
+		s2 = "";
+
+	if (n > strlen(s2))
+
+		cat = malloc(strlen(s1) + strlen(s2)+ 1);
+	else
+		cat = malloc(strlen(s1) + n  + 1);
+
+	if (cat == NULL)
+
+		return (NULL);
+	for(i = 0; s1[i]; ++i)
+		cat[i] = s1[i];
+
+	for(j = 0; s2[j] && j < n; ++j, ++i)
+		cat[i]=s2[j];
+	cat[i] = 0;
+	return (cat);
+}
