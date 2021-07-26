ec851fd89bacb1c6d7b345d17aebd65c9f059b8f
Esoteric918 <3411@holbertonschool.com>
2021-07-26 08:47:15 -0700

    struct dog

---
+++ 0x0F-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _struct_dog_
+#define _struct_dog_
+/**
+ * struct dog - showes info on dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: gives the dog name , age and the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
