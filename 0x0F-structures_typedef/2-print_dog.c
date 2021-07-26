79e49b964f2f7c64ddbca180b97b0cd1516fa117
Esoteric918 <3411@holbertonschool.com>
2021-07-26 12:57:53 -0700

    print strc dog info

--- 0x0F-structures_typedef/2-print_dog.c
+++ 0x0F-structures_typedef/2-print_dog.c
@@ -1,1 +1,25 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - prnt objects in strc dog
+ *@d:ptr to strc dog
+ *
+ *Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("(nil)");
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f \n", d->age);
+	}
+	if (d->owner == NULL)
+		printf ("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n",d->owner);
+}
