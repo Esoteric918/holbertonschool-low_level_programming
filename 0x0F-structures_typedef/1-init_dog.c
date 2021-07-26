61bc9f6164f8f978ad991d62503c6c4986557612
Esoteric918 <3411@holbertonschool.com>
2021-07-26 09:44:41 -0700

    add files

---
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initialize struct dog
+ *@d: ptr to struct dog
+ *@name: ptr to str
+ *@age: age of struct sub
+ *@owner: ptr to str
+ *
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog
+		
+}
