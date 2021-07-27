7c9e69e74a682277941cfd024fd23ba2423b674a
Esoteric918 <3411@holbertonschool.com>
2021-07-27 08:38:56 -0700

    prt func to print name

--- 0x10-function_pointers/0-print_name.c
+++ 0x10-function_pointers/0-print_name.c
@@ -2,12 +2,13 @@
 /**
  * print_name - prints name
  *@name:name to be printed
- *@f: function ptr calling print_name func
+ *@f: function that takes one argument
  *
  *
  *Returns: char ptr
  */
 void print_name(char *name, void (*f)(char *))
 {
-	
+	if (name && f)
+		f(name);
 }
