2d59d5f905e55e8764f2352d0d44738e4bc5eacc
Esoteric918 <3411@holbertonschool.com>
2021-07-26 19:55:23 -0700

    makin new dogs

--- 0x0F-structures_typedef/4-new_dog.c
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -1,1 +1,62 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @src: string to be allocated
+ *
+ *Return: 0
+ */
+char *_strdup(char *src)
+{
+	char *str;
+	char *p;
+	int len = 0;
+
+	if (src == NULL)
+		return (NULL);
+	while (src[len])
+		++len;
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	p = str;
+	while (*str)
+		*p++ = *src++;
+	*p = '\0';
+	return (str);
+}
+/**
+ * *new_dog - init dog_t
+ *@name: name char
+ *@age: age float
+ *@owner: owner char
+ *
+ *Return: NULL if fails and ptr if true
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *newdog;
+
+	newdog = malloc(sizeof(dog_t));
+
+	if (newdog)
+	{
+		newdog->name = _strdup(name);
+		if (newdog->name == NULL)
+		{
+			free(newdog);
+			return (NULL);
+		}
+		newdog->age = age;
+		newdog->owner = _strdup(owner);
+
+		if (newdog->owner == NULL)
+		{
+			free(newdog->name);
+			free(newdog);
+			return (NULL);
+		}
+	}
+	return (newdog);
+}
