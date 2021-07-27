43710a3b4cb4e80c4e9bcce45e2337021f71e52a
Esoteric918 <3411@holbertonschool.com>
2021-07-27 11:23:03 -0700

    make the syntax right

--- 0x10-function_pointers/2-int_index.c
+++ 0x10-function_pointers/2-int_index.c
@@ -1,1 +1,28 @@
 #include "function_pointers.h"
+/**
+ * int_index - returns index of 1st elem for which cmp func doesnt rtn 0
+ * @array: array of int to be srch
+ * @size: num of elem in array
+ * @cmp: ptr to function that compares values
+ *
+ * Return: int
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
