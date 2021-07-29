b4000267176a6c81fd7203c13a9b9bba14ec85bb
Esoteric918 <3411@holbertonschool.com>
2021-07-29 10:27:57 -0700

    make it betty

--- 0x11-variadic_functions/0-sum_them_all.c
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
  * sum_them_all - gets sum of all parameters
  *@n: num of args
- *
+ *Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -16,10 +16,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	if (n == 0)
-		return(0);
+		return (0);
 
 	for (i = 0; i < n; ++i)
-		sum+= va_arg(ap, int);
+		sum += va_arg(ap, int);
 	va_end(ap);
-	return(sum);
+	return (sum);
 }
