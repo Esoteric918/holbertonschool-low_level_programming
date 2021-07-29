3d147234d56cbfba098c0954fba70cf79f24f506
Esoteric918 <3411@holbertonschool.com>
2021-07-29 10:37:09 -0700

    print the strings

---
+++ 0x11-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings  - prints strings
+ *@separator: is the str to be printed between str
+ *@n: number of strs to be passed to the function
+ *
+ *Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	const char *str;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n)
+	{
+		for (i = 0; i < n; ++i)
+		{
+			str = va_arg(ap, const char *);
+			if (*str)
+				printf("%s", str);
+			else
+				printf("(nil)");
+
+			if (separator && i < n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
