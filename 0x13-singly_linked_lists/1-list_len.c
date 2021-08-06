a4df2580463968bcbe4db949285778ac8c1480e4
Esoteric918 <3411@holbertonschool.com>
2021-08-06 13:22:49 -0700

    + correct prototype

--- 0x13-singly_linked_lists/1-list_len.c
+++ 0x13-singly_linked_lists/1-list_len.c
@@ -5,16 +5,12 @@
  * @h: pointer to node list
  * Return: the number of nodes
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	unsigned int ncount = 0;
 
 	while (h)
 	{
-		if (!h->str)
-			;
-		else
-			;
 		ncount++;
 		h = h->next;
 	}
