0c3adfcb87485e1dc0c7ab8544484db516fe6c17
Esoteric918 <85411745+Esoteric918@users.noreply.github.com>
2021-08-06 14:20:21 -0500

    print list t

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints  nodes stored
+ * @h: pointer to node list 
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    if (h == NULL)
+    return(0);
+    
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else 
+        printf("[%u] %s\n", h->len , h->str);
+        h = h->next;
+        count;
+    }
+    return(count); 
+}
