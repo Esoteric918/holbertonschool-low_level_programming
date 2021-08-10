f0068bd1dfc12d176b5167b576546b79fd1629f7
Esoteric918 <3411@holbertonschool.com>
2021-08-10 09:22:21 -0700

    betty happy

--- 0x14-more_singly_linked_lists/0-print_listint.c
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -1,30 +1,29 @@
 #include "lists.h"
-/ **
+/**
  * listint_len - prints all elements of listint_t
- * @h: struct of listint_t 
- * 
+ * @h: struct of listint_t
+ *
  * Return: number of nodes
- * /
- size_t listint_len(const listint_t *h)
- {
-     size_t count= head;
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
 
-     if (h == NULL)
-     return(0);
+	if (h == NULL)
+		return (0);
 
-     while (h != NULL)
-     {
-         if (h->next == '\0')
-         {
-             printf("0\n");
-         }
-         else
-         {
-             printf("%i\n",h->n);
-             h = h->next;
-             count++;
-         }
-         
-     }
-     return (count);
- }
+	while (h != NULL)
+	{
+		if (h->n == '\0')
+		{
+			printf("0\n");
+		}
+		else
+		{
+			printf("%i\n", h->n);
+			h = h->next;
+			count++;
+		}
+	}
+	return (count);
+}
