185b955e0a8639921493b613253fe99eee57844d
Esoteric918 <3411@holbertonschool.com>
2021-08-11 09:11:46 -0700

    refactor syntax

--- 0x14-more_singly_linked_lists/0-print_listint.c
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -14,12 +14,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == '\0')
-		{
-			printf("0\n");
-		}
-		else
-			printf("%i\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
 		count++;
 	}
