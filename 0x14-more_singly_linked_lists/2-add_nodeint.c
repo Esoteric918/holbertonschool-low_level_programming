7fce0b4d9f95d19e4a9cc33e8657d8aab5d5688c
Esoteric918 <3411@holbertonschool.com>
2021-08-10 19:19:57 -0700

    betty

--- 0x14-more_singly_linked_lists/2-add_nodeint.c
+++ 0x14-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
  * add_nodeint - and node to head of list
  * @head: pointer to the head of the list
  * @n: interger being add to head
- * Return: address of the new element, or NULL if it failed
+ * Return: address of the new element - NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
