ac90bf9f2f9fa6ce922cc87f90267f0b79613a6e
Esoteric918 <3411@holbertonschool.com>
2021-08-17 10:20:00 -0700

    fixed betty issue

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -18,8 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDWR);
+
 	if (fd == -1)
-                return (0);
+		return (0);
+
 	rd = read(fd, buf, letters);
 	x = write(STDOUT_FILENO, buf, rd);
 
@@ -28,6 +30,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
-	close (fd);
+	close(fd);
 	return (x);
 }
