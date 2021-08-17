18b91a906347b1bfc7a9c5235fb1005afd753dba
Esoteric918 <3411@holbertonschool.com>
2021-08-17 13:26:39 -0700

    chk if text_content NULL

--- 0x16-file_io/2-append_text_to_file.c
+++ 0x16-file_io/2-append_text_to_file.c
@@ -11,7 +11,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
+		if (text_content == NULL)
+                        return (-1);
 		fd = open(filename, O_RDWR | O_APPEND);
+
 		if (filename == NULL)
 			return (-1);
 		if (fd == -1)
