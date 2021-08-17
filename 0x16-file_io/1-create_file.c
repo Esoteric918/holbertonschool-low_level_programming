201a0c4764b3c8dfcd28359a8a469895d28cc40c
Esoteric918 <3411@holbertonschool.com>
2021-08-17 11:52:07 -0700

    create file

---
+++ 0x16-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: content to be added to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		close(fd);
+		return (1);
+	}
+	for (i = 0; text_content[i] != '\0'; ++i)
+		;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
