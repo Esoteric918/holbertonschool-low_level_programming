1bc34389e3793a5d4eb841eb03ff4c7011393edb
Esoteric918 <3411@holbertonschool.com>
2021-08-17 13:36:43 -0700

    fixed text_content

--- 0x16-file_io/2-append_text_to_file.c
+++ 0x16-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename)
 	{
 		if (text_content == NULL)
-			return (-1);
+			text_content = "";
 		fd = open(filename, O_RDWR | O_APPEND);
 
 		if (filename == NULL)
