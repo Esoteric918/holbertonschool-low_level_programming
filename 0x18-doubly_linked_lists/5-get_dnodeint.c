48969b6bdfad4d734c2a312c3001c701aa87082a
Esoteric918 <3411@holbertonschool.com>
2021-09-08 07:56:08 -0700

    fix return

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -8,14 +8,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while (head != NULL)
-    {
-        if (count == index)
-            return (head->next);
-        count++;
-        head = head->next;
-    }
-    return (NULL);
+	if (head == NULL)
+		return(NULL);
+
+	while (head != NULL)
+	{
+		if (count == index)
+			return (head);
+		count++;
+		head = head->next;
+	}
+	return (head);
 }
