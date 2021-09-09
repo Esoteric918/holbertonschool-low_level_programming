5fa0504267f42b6e81046ff38688bcf67c63fed2
Esoteric918 <3411@holbertonschool.com>
2021-09-09 06:35:56 -0700

    add chek for temp next null

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,10 +31,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new->next = temp->next;
 			if (temp->next != NULL)
-			{
 				temp->next->prev = new;
-				temp->next = new;
-			}
+			temp->next = new;
 		}
 		else
 		{
