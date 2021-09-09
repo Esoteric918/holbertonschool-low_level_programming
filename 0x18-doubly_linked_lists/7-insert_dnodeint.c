daa7fe75bf91f570efa4ca8770ccf582d70dc0cc
Esoteric918 <3411@holbertonschool.com>
2021-09-08 19:44:37 -0700

    undo

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,13 +39,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-/*	else
+	else
 	{
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
-		return (NULL);
+			return (NULL);
 		new->n = n;
 		*h = new;
-	}*/
+	}
 	return (new);
 }
