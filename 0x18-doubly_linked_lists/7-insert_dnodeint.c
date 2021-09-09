5b173ee3225c962e0abc3a77fe65de8dd63848be
Esoteric918 <3411@holbertonschool.com>
2021-09-08 20:55:57 -0700

    fix return value

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
-		if (!idx)
+		if (idx == 0)
 			return (add_dnodeint(h, n));
 		temp = *h;
 		for (x = 0; temp && x < idx - 1; x++)
