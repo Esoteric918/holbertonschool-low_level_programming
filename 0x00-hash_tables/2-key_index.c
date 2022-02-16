4ab6b5f8d8a6b6ace0200d29343d1ef9ff707e04
Esoteric918 <3411@holbertonschool.com>
2022-02-16 11:33:36 -0600

    betty fix

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -4,9 +4,9 @@
  *
  * @key: is the key
  * @size: -  is the size of the array of the hash table
- * @Return unsigned long int
+ * Return: unsigned long int
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return hash_djb2(key) % size;
+	return (hash_djb2(key) % size);
 }
