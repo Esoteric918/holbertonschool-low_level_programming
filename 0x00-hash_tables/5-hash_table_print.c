172e5df33e546001bf22f1c1b2fbefda7265597b
Esoteric918 <3411@holbertonschool.com>
2022-02-17 15:33:23 -0600

    task 5 kinda

---
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print key/value of hashTable
+ * @ht: table to be printed
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *node;
+
+	if (!ht)
+		return;
+	printf("{");
+
+	for (; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+		while (node)
+		{
+			printf("'%s': '%s, '", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
