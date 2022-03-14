c0a5eab0fd329673a0d9471e706a30f05fb6a84a
Esoteric918 <3411@holbertonschool.com>
2022-03-14 14:49:10 -0500

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,12 +10,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int i;
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%i] = [%i]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
-    return -1;
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
