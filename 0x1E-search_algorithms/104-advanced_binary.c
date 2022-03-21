b4400ce58223a296af50305b0a4880c6f6cabacd
Esoteric918 <3411@holbertonschool.com>
2022-03-21 11:07:06 -0500

    add file  update header file

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+/**
+ * advanced_binary searches for a value in a sorted array of integers
+ * @array: the array to be searched
+ * @size: the size of the array
+ * @value: the value to be searched for in the array
+ *
+ * Return the value and the index of
+ */
+
+
+int advanced_binary(int *array, size_t size, int value)
