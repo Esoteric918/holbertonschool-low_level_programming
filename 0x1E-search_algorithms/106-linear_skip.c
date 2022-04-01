26e1a3e9e98d451191cad5ee5a9fd42990cc1429
Esoteric918 <3411@holbertonschool.com>
2022-04-01 09:45:29 -0500

    betty really happy

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -4,7 +4,7 @@
  *
  * @list: list to be searched
  * @value: value to be found
- * @return skiplist_t*
+ * Return: NULL
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
@@ -13,16 +13,17 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (!list)
 		return (NULL);
 
-    while (list->n < value && list->express)
-    {
-        runner =list;
-        list = list->express;
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-    }
-    if (value <= list->n)
+	while (list->n < value && list->express)
+	{
+		runner = list;
+		list = list->express;
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+	}
+	if (value <= list->n)
 
-        printf("Value found between indexes [%lu] and [%lu]\n", runner->index, list->index);
-    else
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				runner->index, list->index);
+	else
 	{
 		runner = runner->express;
 		while (list->next)
